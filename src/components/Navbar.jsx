3f9e950c5ef4df563509a255444a6b17a24c886d
TobiasBurman <113107721+TobiasBurman@users.noreply.github.com>
2023-10-02 13:43:53 +0200

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import navBarStyles from "./navbar.module.css"
+import React from "react";
+import navBarStyles from "./navbar.module.css";
+import logo from "../assets/slan.png";
 
 export default function Navbar() {
   return (
-    <ul className={navBarStyles.ul}>
-      <li className={navBarStyles.li}><a href='#'>Home</a></li>
-      <li className={navBarStyles.li}><a href='#'>News</a></li>
-      <li className={navBarStyles.li}><a href='#'>Contact</a></li>
-      <li className={navBarStyles.li}><a href='#'>About</a></li>
-    </ul>
-  )
+    <>
+      <ul className={navBarStyles.ul}>
+        
+          <img src={logo} alt="Logo" className={navBarStyles.img} />
+      
+        <li className={navBarStyles.li}>
+          <a href="#">Home</a>
+        </li>
+        <li className={navBarStyles.li}>
+          <a href="#">News</a>
+        </li>
+        <li className={navBarStyles.li}>
+          <a href="#">Contact</a>
+        </li>
+        <li className={navBarStyles.li}>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </>
+  );
 }
