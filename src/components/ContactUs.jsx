2c0456eb4933d4f9ee4fd649824fd0b9aa5e694f
Fredrik Eriksson <f.eriksson86@gmail.com>
2023-10-02 18:12:18 +0200

    ContactForm

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -4,10 +4,12 @@ import ContactStyles from './ContactUs.module.css'
 export default function ContactUs() {
   return (
     <>
-    <div>Contact us for tryout</div>
     <div className={ContactStyles.container}>
+    <div className={ContactStyles.title}>Contact us for tryout</div>
         <div className={ContactStyles.container2}>
-
+          <div className={ContactStyles.forms}>
+            <input type="text" />
+          </div>
         </div>
     </div>
     </>
