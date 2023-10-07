e406822499189abff7f921476bf30d8446b31e25
TobiasBurman <113107721+TobiasBurman@users.noreply.github.com>
2023-10-08 00:32:51 +0200

    bangerContent

--- src/components/BangerContent.jsx
+++ src/components/BangerContent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BangerContentStyles from "./BangerContent.module.css";
-
+import ReactPlayer from "react-player";
 function BangerContent() {
   return (
     <div className={BangerContentStyles.wrapper} id="bangerContent">
@@ -9,7 +9,19 @@ function BangerContent() {
         <br />
       </div>
       <div className={BangerContentStyles.videoList}>
-        <div className={BangerContentStyles.videoBox}>Box1</div>
+        <div className={BangerContentStyles.videoBox}>
+          Box1
+          <div className={BangerContent.videoContent}>
+            <p>1v2 from ...</p>
+            <div className={BangerContent.videoPlayer}>
+            <ReactPlayer
+              url="<https://www.youtube.com/watch?v=7sDY4m8KNLc>
+              "
+              width="560" height="515"
+            />
+            </div>
+          </div>
+        </div>
         <div className={BangerContentStyles.videoBox}>Box2</div>
         <div className={BangerContentStyles.videoBox}>Box3</div>
         <div className={BangerContentStyles.videoBox}>Box4</div>
