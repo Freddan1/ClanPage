53c173d5659fb28d2a76d587aeccf875e1ba52c5
Fredrik Eriksson <f.eriksson86@gmail.com>
2023-10-09 10:20:00 +0200

    Contact form sends email to slancs1337@gmail.com

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,43 +1,57 @@
 import React from 'react'
 import ContactStyles from './ContactUs.module.css'
 import SubmitButton from './SubmitButton'
+import emailjs from '@emailjs/browser'
 
 export default function ContactUs({id}) {
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_774arth', 'template_a9f24d6', e.target, '4nUCQ-TwvO8xnooo_' )
+  }
+
   return (
     <>
     <div className={ContactStyles.container }id={id}>
       <div className={ContactStyles.formContainer} >
         <div className={ContactStyles.text}>Contact us for tryout</div>
-            <form action="#">
+            <form action="#" onSubmit={sendEmail}>
               <div className={ContactStyles.formRow}>
                 <div className={ContactStyles.inputData}>
-                  <input type="text" required/>
+                  <input type="text" name='first_name' required/>
                   <div className={ContactStyles.underline}></div>
-                  <label htmlFor="">First Name</label>
+                  <label htmlFor="first_name">First Name</label>
                 </div>
                 <div className={ContactStyles.inputData}>
-                  <input type="text" required/>
+                  <input type="text" name='last_name' required/>
                   <div className={ContactStyles.underline}></div>
-                  <label htmlFor="">Last Name</label>
-                </div>
-              </div>
-              <div className={ContactStyles.formRow}>
-                <div className={ContactStyles.inputData}>
-                  <input type="text" required/>
-                  <div className={ContactStyles.underline}></div>
-                  <label htmlFor="">Email Adress</label>
+                  <label htmlFor="last_name">Last Name</label>
                 </div>
               </div>
               <div className={ContactStyles.formRow}>
                 <div className={ContactStyles.inputData}>
-                  <textarea className={ContactStyles.textArea} cols="80" rows="8" required></textarea>
+                  <input type="text" name='email_from' required/>
+                  <div className={ContactStyles.underline}></div>
+                  <label htmlFor="email_from" name="email__from">Email Adress</label>
+                </div>
+              </div>
+              <div className={ContactStyles.formRow}>
+                <div className={ContactStyles.inputData}>
+                  <textarea 
+                  className={ContactStyles.textArea} 
+                  cols="80" 
+                  rows="8" 
+                  required
+                  name='message'
+                  ></textarea>
                   <br />
                   <div className={ContactStyles.underline1}></div>
-                  <label htmlFor=""> Write your message</label>
+                  <label htmlFor="" id='message'> Write your message</label>
                   <br />
                 </div>
               </div>
-                <SubmitButton/>
+                <SubmitButton />
             </form>
           </div>
       </div>
